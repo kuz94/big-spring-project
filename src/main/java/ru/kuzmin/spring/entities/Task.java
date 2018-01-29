8cd6ef6985f463f88e67905a2d45ac46bfe47041
Anton <kuz942@gmail.com>
2018-01-29 22:04:59 +0300

    Добавлен javadoc к сущности Task

--- src/main/java/ru/kuzmin/spring/entities/Task.java
+++ src/main/java/ru/kuzmin/spring/entities/Task.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ru.kuzmin.spring.entities;
 
 /**
- *
+ * A {@code Task} represents task entity from database.
+ * Also it is ssh test
+ * 
  * @author kuzmin_a_a
  */
 public class Task {
